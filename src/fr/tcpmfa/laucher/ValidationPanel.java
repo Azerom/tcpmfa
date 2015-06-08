11f4e57ba0719cfe6d8ae32c26ddf22533c23d9d
Azerom <az3rom@gmail.com>
2015-06-08 15:05:59 +0200

    Update ValidationPanel

--- src/fr/tcpmfa/laucher/ValidationPanel.java
+++ src/fr/tcpmfa/laucher/ValidationPanel.java
@@ -1,7 +1,37 @@
 package fr.tcpmfa.laucher;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
 import javax.swing.JPanel;
 
 public class ValidationPanel extends JPanel {
+	
+	public ValidationPanel (){
+		super();
+		
+		JButton launchBouton = new JButton("Launch");
+
+		launchBouton.addActionListener(new ActionListener(){
+
+			public void actionPerformed(ActionEvent arg0) {        
+				System.out.println("Lanchement ! Achtung !");
+				setVisible(false);
+
+			}  
+
+		});
+		
+		
+		JButton loadButton = new JButton("Load");
+		loadButton.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent arg0) {
+				System.out.println("Chargement de partie");
+				setVisible(false);
+			}
+		});
+	}
 
 }
