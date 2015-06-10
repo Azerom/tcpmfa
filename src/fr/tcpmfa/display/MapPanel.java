d1d70da5ec93f79aefc7f3b3f7318d07681c1132
Deadox <Deadox@10.162.129.7>
2015-06-10 16:24:15 +0200

    Update Jpanel MapPanel

--- src/fr/tcpmfa/display/MapPanel.java
+++ src/fr/tcpmfa/display/MapPanel.java
@@ -2,18 +2,35 @@ package fr.tcpmfa.display;
 
 import java.awt.BorderLayout;
 import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
 
+import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class MapPanel {
+public class MapPanel extends JPanel{
+	
  private Image background;
 
- public void setBackground(Image Image){
-
- }
-
+public void Background(){
+	if (ID_map = 1){
+			JLabel image = new JLabel( new ImageIcon("Images/Map1F.png"));
+	}
+	else {
+	if (ID_map = 2){
+		JLabel image = new JLabel( new ImageIcon("Images/Map2F.png"));
+	else {
+		JLabel image1 = new JLabel( new ImageIcon("Images/Map3F.png"));
+			}
+	}
+	}
+	
 }
-
-
+}
+/*
+ **Affiche MAP 1 quand ID_map = 1
+ **Affiche MAP 2 quand ID_map = 2
+ **Affiche MAP 3 quand ID_map = 3
+ */
