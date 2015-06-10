7b4e7d2b57c98b08b37f7b7c5a74f8f3a3608cf2
Deadox <Deadox@10.162.129.7>
2015-06-10 10:59:38 +0200

    Update Map & MapPanel

--- src/fr/tcpmfa/display/Map.java
+++ src/fr/tcpmfa/display/Map.java
@@ -1,14 +1,18 @@
 package fr.tcpmfa.display;
 
+import java.awt.Image;
+
 public class Map {
 
+	
+	private Image Image;
 	public Image getImage(){
-		
+		return Image;
 	}
 	public void setImage(Image Image){
-		
+		this.Image = Image;
 	}
-	public void addGrahicalElement(GraphicalElement){
+	public void addGrahicalElement(GraphicalElement Element1){
 		
 	}
 }
--- src/fr/tcpmfa/display/MapPanel.java
+++ src/fr/tcpmfa/display/MapPanel.java
@@ -1,10 +1,12 @@
 package fr.tcpmfa.display;
 
+import java.awt.Image;
+
 public class MapPanel {
  private Image background(){
-	 
+	return null;
  }
- public void setBackground(Image){
+ public void setBackground(Image Image){
 	
  }
  
