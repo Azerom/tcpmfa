6dece7542fd984532a845beee26cf234a2827d78
XXTower <xx-tower@hotmail.fr>
2015-06-11 17:18:50 +0200

    Finich méthode save

--- src/fr/tcpmfa/engine/Map.java
+++ src/fr/tcpmfa/engine/Map.java
@@ -54,7 +54,7 @@ public class Map {
 		
 		//Test code remove ASAP
 		this.towers = new ArrayList<Tower>();
-		this.towers.add(new Tower(5, null, "Tour", new Coordinate(120, 80), game,0,80,0,5));
+		this.towers.add(new Tower(5, null, "Tour", new Coordinate(120, 80), game,0,80,0,5,0));
 	}
 	
 	public Coordinate getCoordStart() {
